1b831ec2b060ae201d743384ebd584130cb6fd7b
jzead <65839446+jzead@users.noreply.github.com>
2020-05-25 00:21:22 +0900

    Update cat.py

--- cat.py
+++ cat.py
@@ -2,11 +2,6 @@ import discord
 import os
 
 client = discord.Client()
-a = "반가워"
-b = "반가워 아마도..."
-c = "누구세요?"
-A = "놀아줄까?"
-B = "물고기 잡고있다냥"
 
 @client.event
 async def on_ready():
@@ -22,13 +17,13 @@ async def on_message(message):
         await client.change_presence(status=discord.Status.offline)
 
     if message.content.startswith("뭐해"):
-        await message.channel.send(B)
+        await message.channel.send("물고기 잡고있다냥")
 
     if message.content.startswith("심심해"):
-        await message.channel.send(A)
+        await message.channel.send("놀아줄까?")
 
     if message.content.startswith("안녕"):
-        await message.channel.send(a)
+        await message.channel.send("반가워")
 
 
 
